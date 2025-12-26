fabfa9ea48eb70c65a19c05865b72071b1a4ed7b
Amey Salvi <ameypro157@gmail.com>
2025-12-26 13:03:55 +0530

    Commit for Adding Docker and Jenkins Files

---
+++ Jenkins.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_IMAGE = "devops-ci-cd-aws"
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/ameysalvi5675/devops-ci-cd-aws.git'
+            }
+        }
+
+        stage('Maven Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+
+        stage('Docker Build') {
+            steps {
+                sh 'docker build -t $DOCKER_IMAGE:latest .'
+            }
+        }
+
+        stage('Docker Push') {
+            steps {
+                withCredentials([usernamePassword(
+                    credentialsId: 'dockerhub-creds',
+                    usernameVariable: 'USER',
+                    passwordVariable: 'PASS'
+                )]) {
+                    sh '''
+                    docker login -u $USER -p $PASS
+                    docker push $DOCKER_IMAGE:latest
+                    '''
+                }
+            }
+        }
+
+        stage('Deploy to EC2') {
+            steps {
+                sh '''
+                docker stop app || true
+                docker rm app || true
+                docker run -d -p 8080:8080 --name app $DOCKER_IMAGE:latest
+                '''
+            }
+        }
+    }
+}
